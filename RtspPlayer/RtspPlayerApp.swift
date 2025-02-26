c03f505455a4d0616deb38db13c519babdc672ac
Nghi-NV <canary020990@gmail.com>
2025-02-26 17:57:37 +0700

    Initial Commit

---
+++ RtspPlayer/RtspPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RtspPlayerApp.swift
+//  RtspPlayer
+//
+//  Created by Nghi Nguyen on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RtspPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RtspPlayerTests/RtspPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RtspPlayerTests.swift
+//  RtspPlayerTests
+//
+//  Created by Nghi Nguyen on 26/2/25.
+//
+
+import Testing
+@testable import RtspPlayer
+
+struct RtspPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
